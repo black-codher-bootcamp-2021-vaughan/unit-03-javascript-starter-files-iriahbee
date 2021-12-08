0c02ea00cf7fe65ebf2e6468d75b8054be91c51f
iriahbee <solagnelake@gmail.com>
2021-12-08 18:42:18 +0000

    While loop

--- session-6/index.js
+++ session-6/index.js
@@ -1,19 +1,22 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
-const randomNumber = Math.round(Math.random() * 10);
+
+
+
 
 // ****************************************************TASK 1****************************************************
 
 
+const randomNumber = Math.round(Math.random() * 10);
+let counter = 1;
+
+while (counter <= 10) {
 
 
+counter ++ 
 
+}
 
-
-
-
-
-
-
+console.log (randomNumber)
 
 
 
